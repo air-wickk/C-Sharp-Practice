0bb48ce9010c762886d8eb41e0a0c42163c0746c
air-wick <72420242+air-wickk@users.noreply.github.com>
2024-06-11 19:15:12 -0700

    reorganized & some chapter 2

---
+++ Chapter 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console; // This is an import, we use it to import the console library to print to our console
+
+class HelloWorld // This is how we declare a class. Every "file" is a class, and can be named anything
+{
+    static void Main() // This will be explained later. Main() is the execution instructions for your program
+    {
+        WriteLine("Hello, World!"); // Within Main()'s brackets are the instructions. In this case, print "Hello, World!"
+    }
+
+}

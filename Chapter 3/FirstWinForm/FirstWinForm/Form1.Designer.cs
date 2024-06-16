5fc6cde0c7f859944bca3ef98e812fa7f4863ac4
air-wick <72420242+air-wickk@users.noreply.github.com>
2024-06-15 17:13:44 -0700

    added ch3 readme

---
+++ Chapter 3/FirstWinForm/FirstWinForm/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FirstWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Font = new Font("Mont Heavy DEMO", 12F);
+            button1.Location = new Point(256, 252);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 35);
+            button1.TabIndex = 3;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Mont Heavy DEMO", 14F);
+            label1.Location = new Point(138, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 26);
+            label1.TabIndex = 1;
+            label1.Text = "Enter a number:";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(347, 150);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Mont Heavy DEMO", 14F);
+            label2.Location = new Point(72, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(231, 26);
+            label2.TabIndex = 3;
+            label2.Text = "Enter another number:";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(347, 195);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(183, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Mont Heavy DEMO", 14F);
+            label3.Location = new Point(16, 5);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 26);
+            label3.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(186, 307);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(261, 35);
+            panel1.TabIndex = 6;
+            panel1.Paint += this.panel1_Paint;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(660, 454);
+            Controls.Add(panel1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Panel panel1;
+    }
+}
---
+++ Chapter 3/README.txt
@@ -0,0 +1,4 @@
+Chapter 3 has one file because it was a windows form file.
+
+The chapter consisted of familiarizing yourself with GUI using Windows Forms and
+incorporating previously learned knowledge into a simple GUI experience.
